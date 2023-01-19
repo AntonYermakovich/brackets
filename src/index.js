7c6c560a7f982ed3c5a0576346928a44a3da9f74
Anton Yermakovich <antonyermakovich@gmail.com>
2023-01-19 20:08:03 +0300

    feat: write function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let OPEN_BRACKETS = []
+  let BRACKETS_PAIR = {}
+
+  bracketsConfig.forEach(bracket => {
+    BRACKETS_PAIR[bracket[1]] = bracket[0]
+    OPEN_BRACKETS.push(bracket[0])
+  })
+
+  if (str.length % 2 !== 0) return false
+
+  for (let i = 0; i < str.length; i++) {
+    let currentElem = str[i]
+
+    if (stack.includes('|') && currentElem === '|'
+      || stack.includes('8') && currentElem === '8'
+      || stack.includes('7') && currentElem === '7') {
+      stack.pop()
+      continue
+    }
+
+    if (OPEN_BRACKETS.includes(currentElem)) {
+      stack.push(currentElem)
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+
+      let top = stack[stack.length - 1]
+
+      if (BRACKETS_PAIR[currentElem] === top) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+
+  return stack.length === 0
 }
